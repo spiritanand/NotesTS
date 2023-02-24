a6486a1d05d3f4da2c39cf8be17c40242f118fac
Suryansh Anand <suryansh.anand42@gmail.com>
2023-02-24 20:29:43 +0530

    Added url params in edit and show components

--- src/pages/EditNote.tsx
+++ src/pages/EditNote.tsx
@@ -1,6 +1,10 @@
 import React from "react";
+import {useParams} from "react-router-dom";
 
 const EditNote = () => {
+  const {id} = useParams();
+  // const note =
+  
   return (
 	<div>
 	  EditNote
